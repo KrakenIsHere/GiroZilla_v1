1a8c1e971a39654242aac6f69ee579388aafa43d
Kristian K. S. Nielsen <kris9463@edu.sde.dk>
2020-01-17 14:06:03 +0100

    Update

--- GiroZilla/MainWindow.xaml.cs
+++ GiroZilla/MainWindow.xaml.cs
@@ -43,7 +43,7 @@ namespace GiroZilla
             InitializeComponent();
 
             VerifyLogsFolder();
-            CheckLicense();
+            //CheckLicense();
             SetTitle();
         }
 
