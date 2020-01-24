844cde72e1ce79e5022a458987b34c4790b4c43a
Kristian K. S. Nielsen <kris9463@edu.sde.dk>
2020-01-24 10:39:28 +0100

    Update
    Minor visual changes

--- GiroZilla/Properties/AssemblyInfo.cs
+++ GiroZilla/Properties/AssemblyInfo.cs
@@ -18,6 +18,6 @@ using System.Windows;
 
 [assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
 
-[assembly: AssemblyVersion("1.0.795")]
-[assembly: AssemblyFileVersion("1.0.795")]
-[assembly: AssemblyInformationalVersion("1.0.795")]
+[assembly: AssemblyVersion("1.0.799")]
+[assembly: AssemblyFileVersion("1.0.799")]
+[assembly: AssemblyInformationalVersion("1.0.799")]
