24423cab9e4a138114809813f2d898ef02579812
Kristian K. S. Nieslen <kraken122@live.dk>
2020-07-14 21:58:34 +0200

    Update
    New info

--- GiroZilla/Properties/AssemblyInfo.cs
+++ GiroZilla/Properties/AssemblyInfo.cs
@@ -18,6 +18,6 @@ using System.Windows;
 
 [assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
 
-[assembly: AssemblyVersion("1.3.979")]
-[assembly: AssemblyFileVersion("1.3.979")]
-[assembly: AssemblyInformationalVersion("1.3.979")]
+[assembly: AssemblyVersion("1.3.980")]
+[assembly: AssemblyFileVersion("1.3.980")]
+[assembly: AssemblyInformationalVersion("1.3.980")]
